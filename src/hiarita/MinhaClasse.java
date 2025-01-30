5c42d9a0a814e752af8ceb1995543056e08e3d11
fehiaritaGitHubgit <fehiarita.15@gmail.com>
2025-01-30 20:42:52 -0300

    commit das aulas
    estudo dos tipos primitivas e suas características

---
+++ src/hiarita/MinhaClasse.java
@@ -0,0 +1,16 @@
+package hiarita;
+public class MinhaClasse {
+    public MinhaClasse() {
+    }
+
+    public static void main(String[] args) {
+        String primeiroNome = "Maria";
+        String segundoNome = "Fernanda";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método: " + primeiroNome + " " + segundoNome;
+    }
+}
---
+++ src/hiarita/TiposVariaveis.java
@@ -0,0 +1,20 @@
+package hiarita;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        double salario = 2500;
+        float pi = 3.14F;
+        long cpf = 98765432109L;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5;
+        numero =  10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+    }
+}
+
