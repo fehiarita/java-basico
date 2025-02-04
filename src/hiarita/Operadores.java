d1e433c9abf1b132cceb0c8f8ba5d910aad24b0d
fehiaritaGitHubgit <fehiarita.15@gmail.com>
2025-02-04 18:17:39 -0300

    operadores básicos
    Estudo da utilização de operadores básicos

---
+++ src/hiarita/Operadores.java
@@ -0,0 +1,28 @@
+package hiarita;
+
+public class Operadores {
+public static void main(String[] args) {
+    double soma = 2.5 + 2.5;
+    int subtração = 4 - 1;
+    int multiplicação = 20 * 7;
+    int divisão = 15 / 3;
+//modulo mostra se vai ter resto na divisão
+    int modulo = 18 % 3;
+    double result = (10 * 7) + (20/4);
+    String nome = "eunice" + "bezerra";
+    System.out.println(nome);
+
+    String concatenacao = "?";
+    concatenacao = 1+1+1+"1";
+    System.out.println(concatenacao);
+    concatenacao = 1+"1"+1+1;
+    System.out.println(concatenacao);
+    concatenacao = 1+"1"+1+"1";
+    System.out.println(concatenacao);
+    concatenacao = "1"+1+1+1;
+    System.out.println(concatenacao);
+    concatenacao = "1"+(1+1+1);
+    System.out.println(concatenacao);
+
+}
+}
