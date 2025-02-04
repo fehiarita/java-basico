9deaaba1679583a1af7edd0402cd69803ef27bc4
fehiaritaGitHubgit <fehiarita.15@gmail.com>
2025-02-04 19:11:18 -0300

    estudos de operadores mais aprofundado
    Estudos de incremento, decremento e mudança de variáveis booleanas

--- src/hiarita/Operadores.java
+++ src/hiarita/Operadores.java
@@ -2,14 +2,14 @@ package hiarita;
 
 public class Operadores {
 public static void main(String[] args) {
-    int numero = 5;
-    numero = - numero;
-    //esse operador de negação não alterou o valor da variável numero
-    System.out.println(numero);
-    //mlehor forma de deixar a variável positiva
-    numero = numero * -1;
+     boolean variavel = true;
 
-    System.out.println(numero);
+    //com esse código a variável nao vai ser mudada
+    //  System.out.println(!variavel);
 
+    //forma de inverter uma variável booleana 
+    variavel = !variavel;
+
+     System.out.println(variavel);
 }
 }
