f27f5d7fda9f4e61544e9ac79d7bad8c8403a02a
fehiaritaGitHubgit <fehiarita.15@gmail.com>
2025-02-04 18:37:28 -0300

    operadores unários

--- src/hiarita/Operadores.java
+++ src/hiarita/Operadores.java
@@ -2,27 +2,14 @@ package hiarita;
 
 public class Operadores {
 public static void main(String[] args) {
-    double soma = 2.5 + 2.5;
-    int subtração = 4 - 1;
-    int multiplicação = 20 * 7;
-    int divisão = 15 / 3;
-//modulo mostra se vai ter resto na divisão
-    int modulo = 18 % 3;
-    double result = (10 * 7) + (20/4);
-    String nome = "eunice" + "bezerra";
-    System.out.println(nome);
+    int numero = 5;
+    numero = - numero;
+    //esse operador de negação não alterou o valor da variável numero
+    System.out.println(numero);
+    //mlehor forma de deixar a variável positiva
+    numero = numero * -1;
 
-    String concatenacao = "?";
-    concatenacao = 1+1+1+"1";
-    System.out.println(concatenacao);
-    concatenacao = 1+"1"+1+1;
-    System.out.println(concatenacao);
-    concatenacao = 1+"1"+1+"1";
-    System.out.println(concatenacao);
-    concatenacao = "1"+1+1+1;
-    System.out.println(concatenacao);
-    concatenacao = "1"+(1+1+1);
-    System.out.println(concatenacao);
+    System.out.println(numero);
 
 }
 }
