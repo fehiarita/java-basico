885b9c313bdfd5509c36fbda437781be04566427
fehiaritaGitHubgit <fehiarita.15@gmail.com>
2025-03-18 20:29:53 -0300

    aprendendo executar no terminal
    utilizei meus conhecimentos que aprendi para executar um arquivo .class no terminal

---
+++ src/hiarita/MinhaClasse/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Estou sendo executado pelo terminal! Muito bom!!! Legal dmssss!!!");
+    }
+}
